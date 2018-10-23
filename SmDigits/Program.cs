2650fce8070741fcb47f5e57db49bf59dbb8f901
Alejandro Ortiz <alejandro.ortiz@unosquare.com>
2018-10-23 15:15:09 -0500

    Add project files.

---
+++ SmDigits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
